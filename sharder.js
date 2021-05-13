8c96418a26a328dcd5166112ab852498481e2f20
Byinfazci <84033400+Byinfazci@users.noreply.github.com>
2021-05-13 14:48:22 +0300

    Update sharder.js

--- sharder.js
+++ sharder.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require("discord.js");
 const manager = new ShardingManager("./index.js", {
-    token: require("./config").token,
+    token: require("ODQxNzM0NTY1MDM4Nzg0NTIz.YJrEZg.DOxEZLkbreiJ-CFtd3ChNChw0u0").token,
     totalShards: require("./config").shardCount,
     shardArgs: [ ...process.argv, ...[ "--sharded" ] ]
 });
